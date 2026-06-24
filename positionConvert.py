ce0e77b7eb89143941da59d6d5db8abd12f3f981
a0a7 <a0@ieee.org>
2026-06-24 13:09:34 -0500

    update positionConvert

--- positionConvert.py
+++ positionConvert.py
@@ -1,4 +1,6 @@
 import chess
+import chess.pgn
+import io
 from datasets import load_from_disk
 
 ds = load_from_disk("data/tournament-games")
@@ -6,11 +8,18 @@ ds = load_from_disk("data/tournament-games")
 with open("data/tournament_positions.jsonl", "w") as f:
 
     for game in ds:
-        board = chess.Board()
 
-        for move in game["moves"]:
-            board.push_san(move)
+        pgn = io.StringIO(game["movetext"])
+        parsed = chess.pgn.read_game(pgn)
+
+        if parsed is None:
+            continue
+
+        board = parsed.board()
+
+        for move in parsed.mainline_moves():
+            board.push(move)
 
             f.write(
-                '{"fen": "' + board.fen() + '"}\n'
+                '{"fen":"' + board.fen() + '"}\n'
             )
